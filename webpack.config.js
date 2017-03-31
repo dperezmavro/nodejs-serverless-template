13bff9e1d932c032713ed86adb5eb4c80c4ef3bb
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2017-03-31 11:55:00 +0100

    updated to use latest node version

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: [
-            'es2015',
-            'stage-0'
+            'es2015'
           ]
         }
       }
